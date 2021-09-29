7d3a5b52503fecafd4c374bf57d2ab510016c114
tpris <priscill@live.fr>
2021-09-29 07:56:10 +0200

    ex 2.4 fait

--- TD2/execute.c
+++ TD2/execute.c
@@ -17,20 +17,28 @@ void verifier(int cond, char *s)
     }
 }
 
-int systeme(char* commande){
+
+
+int main(int argc, char **argv)
+{
+    //verifier(argc==1,"Entrez une commande");
+    char *argum[argc];
+    for(int i = 1; i<argc; i++){
+        argum[i-1] = argv[i];
+        printf("tab %d: %s",i,argv[i]);
+    }
+    argum[argc-1] = NULL;
+
     pid_t pid = fork ();
     if (pid) { // father
+        printf("*** execution\n");
         int status;
         wait (&status);
         printf ("retour enfant : %d\n", WEXITSTATUS (status));
     } else {//child
-        execlp(commande,commande,NULL);
+        execvp(argum[0],argum);
         perror("exec");
-        exit(0);
+        return 0;
     }
-}
-
-int main(int argc, char **argv)
-{
-    return systeme(argv[1]);
+    return 0;
 }
---
+++ TD2/system2.c
@@ -0,0 +1,46 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <string.h> 
+
+void verifier(int cond, char *s)
+{
+    if (!cond)
+    {
+        perror(s);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    char buf[10];
+    char dir[50];
+    do{
+        getcwd(dir, sizeof(dir));
+        printf("Priscilla@cmd:%s$ ",dir);
+        fflush(stdout);
+        read(STDIN_FILENO, buf, 10);
+        //fgets(buf,10,stdin);
+        int i = 0;
+        while(buf[i]!='\0' && i<10){
+            if(buf[i]=='\n') buf[i]='\0';
+            i++;
+        }
+        //printf("entrée : %s\n",buf);
+        if(strcmp(buf,"\\q")){
+            if(fork()==0){
+                execlp(buf,buf,NULL);
+                perror("exec");
+                exit(0);
+            }
+            else{wait(0);}
+        }
+    }while(strcmp(buf,"\\q"));
+    return 0;
+}
