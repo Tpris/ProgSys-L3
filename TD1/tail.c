358ac0add8f320fdbc72c570550284f845e54e39
tpris <priscill@live.fr>
2021-09-29 22:11:12 +0200

    tail en cours

--- TD1/tail.c
+++ TD1/tail.c
@@ -29,22 +29,28 @@ int tailRegularFile(int inputFD, int outputFD, int numLines)
         // read(inputFD,c,numLines);
         // write(outputFD,c,numLines);
         char c [TAILLE];
-        read(inputFD,c,TAILLE);
-        int i = TAILLE-1;
+        int size = lseek(inputFD, TAILLE*-1, SEEK_END);
+        off_t i = 0;
         int cpt= 0;
-        while(i>=0 && cpt < numLines){
-            if(c[i]=='\n') cpt++;
-            i--;
+        int nbTaille = 0;
+        while(read(inputFD,c,TAILLE)>0 && cpt < numLines){
+          nbTaille++;
+          while(cpt < numLines){
+              if(c[i]=='\n') cpt++;
+              i++;
+          }
+          lseek(inputFD, TAILLE*-1, SEEK_CUR);
         }
-        int tmp = i;
-        char * sortie = c[i];
-        // for(i; i<TAILLE;i++){
-        //     sortie[i-tmp] = c[i]; 
-        // }
-        write(outputFD,sortie,TAILLE-i);
-        return 0;
+        lseek(inputFD,(nbTaille*TAILLE-i)*-1,SEEK_END);
+        printf("test %ld",i*-1);
+        int r,w;
+        char ch[1];
+        while((r=read(inputFD,ch,1))>0){// 0 = lecture clavier, 1 = sortie, 2 = erreur
+          w = write(outputFD,ch,1);
+          verifier(w==1,"write out");
+        }
     }
-    return 1;
+    return 0;
 }
 
 
@@ -55,7 +61,7 @@ int main(int argc, char *argv[])
     int in = open(argv[1],O_RDONLY);
     verifier(in != -1, argv[1]);
 
-    int out = open(argv[2],O_WRONLY |O_CREAT , 0640); // 640 = droit : user 110 grp 100 other 000
+    int out = open(argv[2],O_WRONLY | O_TRUNC |O_CREAT , 0640); // 640 = droit : user 110 grp 100 other 000
     verifier(out != -1, "IDX");
 
     int pos = atoi(argv[3]);
