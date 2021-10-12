444cd4f803baf7c315ffbb02f346e0f3a3d77ae6
ptissot <priscilla.tissot@u-bordeaux.fr>
2021-10-12 18:10:34 +0200

    logg

--- TD3/log.c
+++ TD3/log.c
@@ -6,6 +6,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <string.h>
 
 void verifier(int cond, char *s){
   if (!cond){
@@ -24,12 +25,37 @@ int main(int argc, char *argv[])
     int tube[2];
     pipe (tube);
 
-    int sortie = dup(stdout);
+    int sortie = dup(1);
 
-    dup2(tube[1],1); close(tube[1]);
 
     // manip fichier 
+    if(fork()==0){
+      close (tube[0]);
+      dup2 (tube[1], STDOUT_FILENO); close (tube[1]);
+        execvp(argv[2],argv+2);
+        perror("exec");
+        exit(1);
+    }
 
-    execvp(argv[2],argv+2);
+     if (fork() == 0)
+     {
+ 
+       close(tube[1]); /* close write side */
+ 
+       /* read some data and print the result on screen */
+       while (read(tube[0], out, 1) != 0)
+         printf("%s", out);
+ 
+       close(tube[0]); /* close the pipe */
+     }
+
+    // wait(0);
+    // close (tube[1]);
+    // //dup2 (tube[R], STDIN_FILENO); close (tube[R]);
+    // write(sortie,tube,sizeof(tube));
+    // //printf("%s",tube[0]);
+
+    return 0;
+    
 
 }
